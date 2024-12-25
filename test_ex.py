4ad0de200ad9d93b3cfeb1031f4ba14a716f757c
y-fukuoka <y-fukuoka@fujitsu.com>
2024-12-25 13:30:49 +0900

    aaaaaaaaaaaaaa

--- test_ex.py
+++ test_ex.py
@@ -8,8 +8,6 @@ from ex import ex_funcs
     "x, y, expected",
     [
         (3, 2, 1),
-        (3, 3, 0),
-        (3, 4, -1),
     ],
 )
 def test_ex_funcs(x: int, y: int, expected: int) -> None:

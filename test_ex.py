6ef6aacc9db5322bfa6fc083fed59c9d033056bc
y-fukuoka-8 <y-fukuoka@fujitsu.com>
2024-12-25 13:36:34 +0900

    Update test_ex.py

--- test_ex.py
+++ test_ex.py
@@ -1,12 +1,13 @@
 import pytest
 from ex import ex_funcs
 
-
 @pytest.mark.parametrize(
     "x, y, expected",
     [
         # この部分を変更
+        #PDPM
         (3, 2, 1)
+        (10, 9, 8)
     ],
 )
 def test_ex_funcs(x: int, y: int, expected: int) -> None:

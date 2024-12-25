c5147fd2a24ec6bbc3d98499490f02ba2048e993
y-fukuoka-8 <y-fukuoka@fujitsu.com>
2024-12-25 13:16:16 +0900

    Update test_ex.py

--- test_ex.py
+++ test_ex.py
@@ -1,15 +1,13 @@
 import pytest
 from ex import ex_funcs
 
-# コンフリクトを起こす
 
 
 @pytest.mark.parametrize(
     "x, y, expected",
     [
-        (3, 2, 1),
-        (3, 3, 0),
-        (3, 4, -1),
+        # この部分を変更
+        (3,2,1)
     ],
 )
 def test_ex_funcs(x: int, y: int, expected: int) -> None:

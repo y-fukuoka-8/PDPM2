45f42c46dd42a4386d7b430c5b5e50ae5c9acc8a
y-fukuoka <y-fukuoka@fujitsu.com>
2024-12-25 13:39:34 +0900

    bababab

--- test_ex.py
+++ test_ex.py
@@ -1,13 +1,25 @@
 import pytest
 from ex import ex_funcs
 
+
 @pytest.mark.parametrize(
     "x, y, expected",
     [
         # この部分を変更
-        #PDPM
-        (3, 2, 1)
-        (10, 9, 8)
+        # PDPM
+        (3, 2, 1)(10, 9, 8)
+    ],
+)
+def test_ex_funcs(x: int, y: int, expected: int) -> None:
+    assert ex_funcs(x, y) == expected
+
+
+@pytest.mark.parametrize(
+    "x, y, expected",
+    [
+        # この部分を変更
+        # PDPM
+        (3, 2, 1)(10, 9, 8)
     ],
 )
 def test_ex_funcs(x: int, y: int, expected: int) -> None:
